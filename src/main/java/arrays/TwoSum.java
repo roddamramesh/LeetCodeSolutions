5e1a4736d5205c3f3399177eda1f02221bcb6cbb
rameshroddam <roddam.ramesh@gmail.com>
2019-12-30 23:40:16 -0500

    `Project Setup`

--- src/main/java/arrays/TwoSum.java
+++ src/main/java/arrays/TwoSum.java
@@ -13,13 +13,13 @@ import java.util.HashMap;
 
 /**
  * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
- * <p>
+ *
  * You may assume that each input would have exactly one solution, and you may not use the same element twice.
- * <p>
+ *
  * Example:
- * <p>
+ *
  * Given nums = [2, 7, 11, 15], target = 9,
- * <p>
+ * 
  * Because nums[0] + nums[1] = 2 + 7 = 9,
  * return [0, 1].
  */
@@ -33,7 +33,7 @@ public class TwoSum {
     }
 
     /**
-     * @param nums   Array of Values
+     * @param nums Array of Values
      * @param target Target Value
      * @return Sum of indexes of target value
      */
